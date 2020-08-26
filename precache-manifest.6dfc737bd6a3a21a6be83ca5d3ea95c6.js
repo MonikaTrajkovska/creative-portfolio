248e2fc3225fdff2dd73d440633e9f9c003f606b
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-08-26 15:03:28 +0200

    Updates

---
+++ precache-manifest.6dfc737bd6a3a21a6be83ca5d3ea95c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c7e3a1fb6a40745d044",
+    "url": "/creative-portfolio/static/css/main.3d5649bc.chunk.css"
+  },
+  {
+    "revision": "9c7e3a1fb6a40745d044",
+    "url": "/creative-portfolio/static/js/main.9c7e3a1f.chunk.js"
+  },
+  {
+    "revision": "d8bbc1fd5653468f46bb",
+    "url": "/creative-portfolio/static/js/1.d8bbc1fd.chunk.js"
+  },
+  {
+    "revision": "227f09cdbac150157884",
+    "url": "/creative-portfolio/static/js/runtime~main.227f09cd.js"
+  },
+  {
+    "revision": "cf322c5950fd6b5a386aef5d8bef7165",
+    "url": "/creative-portfolio/static/media/Profile.cf322c59.JPEG"
+  },
+  {
+    "revision": "b0a1a4112f205fbf26086a063d907225",
+    "url": "/creative-portfolio/index.html"
+  }
+];
